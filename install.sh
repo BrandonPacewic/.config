997bb1236897c4cf04d2e3970c31ee9c0e0208df
BrandonPacewic <pacewicbrandon@gmail.com>
2023-11-08 17:51:23 -0800

    Update install script

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 # Run this install script to setup global commands and move other files to their required directories.
 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root."
-  exit
+if [[ "$EUID" -ne 0 ]]; then 
+    echo "Please run as root."
+    exit
 fi
 
 git submodule update --init --recursive
@@ -16,7 +16,7 @@ if ! [[ -f "../.zshrc" ]]; then
 fi
 
 for file in "scripts"/*; do
-    if [ -f "$file" ]; then
+    if [[ -f "$file" ]]; then
         filename=$(basename "$file")
         sudo scripts/bin_link.sh $file $filename
     fi
