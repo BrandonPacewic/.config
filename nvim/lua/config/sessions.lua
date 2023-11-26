ca9dc9dbf831bac6138bf67fe6586016ca7e3913
BrandonPacewic <pacewicbrandon@gmail.com>
2023-11-25 19:51:28 -0800

    Updated session config loading

--- nvim/lua/config/sessions.lua
+++ nvim/lua/config/sessions.lua
@@ -1,17 +1,23 @@
-local Path = require("plenary.path")
+local status_ok_sm, session_manager = pcall(require, "session_manager")
+local status_ok_path, Path = pcall(require, "plenary.path")
+if not status_ok_sm or not status_ok_path then
+    return
+end
 
-require("session_manager").setup({
-  sessions_dir = Path:new(vim.fn.stdpath("data"), "sessions"),
-  path_replacer = "__",
-  colon_replacer = "++",
-  autoload_mode = require("session_manager.config").AutoloadMode.Disabled,
-  autosave_last_session = true,
-  autosave_ignore_not_normal = true,
-  autosave_ignore_dirs = {},
-  autosave_ignore_filetypes = {
+local config = {
+    sessions_dir = Path:new(vim.fn.stdpath("data"), "sessions"),
+    path_replacer = "__",
+    colon_replacer = "++",
+    autoload_mode = require("session_manager.config").AutoloadMode.Disabled,
+    autosave_last_session = true,
+    autosave_ignore_not_normal = true,
+    autosave_ignore_dirs = {},
+    autosave_ignore_filetypes = {
     "gitcommit",
-  },
-  autosave_ignore_buftypes = {},
-  autosave_only_in_session = true,
-  max_path_length = 80,
-})
+    },
+    autosave_ignore_buftypes = {},
+    autosave_only_in_session = true,
+    max_path_length = 80,
+}
+
+session_manager.setup(config)
