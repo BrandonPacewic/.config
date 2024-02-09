71af85247a218e4b96c912e0ae20835a45e44654
BrandonPacewic <pacewicbrandon@gmail.com>
2024-02-09 06:54:35 -0800

    Command to reinstall code into the system path

---
+++ scripts/reset_code.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm -f /usr/local/bin/code
+export PATH="\$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+echo "Visual Studio Code has been reset."
+echo "Please restart your terminal to apply changes."
