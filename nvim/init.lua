60fd9f213d4340af15a9b6f32ed5eb9850fc23a2
BrandonPacewic <pacewicbrandon@gmail.com>
2024-02-29 09:21:42 -0800

    Vim general options

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,4 @@
-local install_path = vim.fn.stdpath("data" .. "/site/pack/packer/start/packer.nvim")
+local install_path = vim.fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
 
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
     packer_bootstrap = vim.fn.system {
@@ -66,24 +66,143 @@ packer.startup(function(use)
 end)
 
 -- General settings
-vim.g.mapleader = " "
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.scrolloff = 8 
+vim.opt.scroll = 20
+vim.opt.wrap = true
+vim.opt.linebreak = true
+vim.opt.breakindent = true
+vim.opt.showbreak = "↪"
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.mouse = "a"
+vim.opt.conceallevel = 0
+vim.opt.concealcursor = "niv"
+
 vim.opt.syntax = "on"
-vim.opt.updatetime = 300
-vim.opt.scrolloff = 5
-vim.opt.encoding = "utf-8"
+vim.opt.synmaxcol = 300
+vim.opt.hlsearch = true
+vim.opt.termguicolors = true
+vim.opt.cursorcolumn = false
+vim.opt.cursorline = false
+vim.opt.colorcolumn = "120"
+vim.opt.spell = true
+vim.opt.spelllang = "en_us"
+vim.opt.spellsuggest = "best"
+
+vim.opt.scrolljump = 1
+vim.opt.title = true
+vim.opt.titlestring = "%<%F%=%l/%L - nvim"
+vim.opt.icon = true
+vim.opt.iconstring = ""
+
+vim.opt.mouse = "a"
+vim.opt.mousemodel = "popup_setpos"
+vim.opt.mousetime = 500
+
+vim.opt.showcmd = true
+vim.opt.showmode = true -- Could change for lualine
+vim.opt.ruler = true
+vim.opt.rulerformat = "%l,%v  %P"
+vim.opt.report = 3
+vim.opt.verbose = 1
+vim.opt.more = true
+vim.opt.confirm = true
+vim.opt.errorbells = true
+vim.opt.visualbell = false
+vim.opt.helplang = "en"
+
+vim.opt.selection = "exclusive"
+vim.opt.selectmode = "mouse,key"
+vim.opt.keymodel = "startsel,stopsel"
+
+vim.opt.undolevels = 1000
+vim.opt.undofile = true
+vim.opt.undodir = os.getenv("HOME") .. "/.local/share/nvim/undodir"
+vim.opt.undoreload = 10000
+vim.opt.wrapmargin = 1
+vim.opt.backspace = "indent,eol,start"
+vim.opt.complete = ".,w,b,u,t,i"
+vim.opt.matchtime = 2
+vim.opt.matchpairs = "(:),{:},[:],<:>"
+
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
+vim.opt.smarttab = true
+vim.opt.softtabstop = 4
 vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = false
+vim.opt.cindent = false
+vim.opt.copyindent = true
+vim.opt.preserveindent = true
+vim.opt.lisp = false
+
+vim.opt.foldenable = false
+
+vim.opt.diff = false
+vim.opt.diffopt = "internal,filler,closeoff"
+
+vim.opt.maxmapdepth = 1000
+vim.opt.timeout = true
+vim.opt.ttimeout = true
+vim.opt.timeoutlen = 1000
+vim.opt.ttimeoutlen = 50
+
+vim.opt.modeline = true
+vim.opt.modelineexpr = true
+vim.opt.modelines = 5
+vim.opt.binary = false
+vim.opt.endofline = true
+vim.opt.endoffile = true
+vim.opt.fixendofline = false
+vim.opt.bomb = false
+vim.opt.fileformat = "unix"
+vim.opt.fileformats = "unix,dos,mac"
+vim.opt.write = true
+vim.opt.writebackup = true
+vim.opt.backup = true
+vim.opt.backupcopy = "auto"
+vim.opt.backupdir = os.getenv("HOME") .. "/.local/share/nvim/backup"
+vim.opt.autowrite = false
+vim.opt.autowriteall = false
+vim.opt.writeany = false
+vim.opt.autoread = true
+vim.opt.fsync = true
+
+vim.opt.directory = os.getenv("HOME") .. "/.local/share/nvim/swap"
+vim.opt.swapfile = true
+vim.opt.updatecount = 100
+vim.opt.updatetime = 300
+
+vim.opt.history = 1000
+vim.opt.fileignorecase = true
+vim.opt.wildignorecase = true
+vim.opt.wildmenu = true
+
+vim.opt.shell = "/bin/zsh"
+vim.opt.warn = true
+
+vim.opt.isfname = "@,48-57,/,.,-,_,+,,,#,$,%,~,="
+vim.opt.isident = "@,48-57,_,192-255"
+vim.opt.iskeyword = "@,48-57,_,192-255"
+vim.opt.isprint = "@,48-57,_,192-255"
+vim.opt.rightleft = false
+vim.opt.revins = false
+vim.opt.allowrevins = true
+vim.opt.aleph = 0
+vim.opt.arabic = false
+vim.opt.langremap = true
+
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+vim.opt.emoji = true
+
 vim.opt.signcolumn = "yes"
-vim.opt.ignorecase = true
-vim.opt.hlsearch = false
-vim.opt.wrap = true
-vim.opt.breakindent = true
-vim.opt.spelllang = "en_us"
-vim.opt.spell = true
+
+vim.g.mapleader = " "
 
 -- Themes
 local onedark = require("onedark")
